ca7e8b65288e4f5f3dc8a01451bf0a2073a038cf
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-04-23 22:52:05 -0300

    Adicionar arquivos de projeto.

---
+++ AppRpgEtec/Services/Request.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace AppRpgEtec.Services
+{
+    public class Request
+    {
+        //metodo para retornar um int, provavelmente utilizado com o uso do ID vindo do banco
+        public async Task<int> PostReturnIntAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
+        //retorna um objeto, desseraliezando ele e trazendo ele normal/ metodo generico
+        public async Task<TResult> PostAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            TResult result = data;
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                result = await Task.Run(() => JsonConvert.DeserializeObject<TResult>(serialized));
+            else
+                throw new Exception(serialized);
+
+            return result;
+        }
+    }
+}
---
+++ AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,90 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+
+        public ICommand AutenticarCommand { get; set; }
+
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+        }
+
+        public void InicializarCommands()
+        {
+            AutenticarCommand = new Command(async () => await AutenticarUsuario());
+        }
+
+        //atributos que são chamadas na view
+        #region AtributosPropriedades
+        private string login = string.Empty;
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string password = string.Empty;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        public async Task AutenticarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.Password = Password; //<-- Password (propriedade da model de usuario) vai para Password (atributo que é chamado na view)
+
+                Usuario uAutenticacao = await uService.PostAutenticarUsuarioAsync(u);
+
+                if (!string.IsNullOrEmpty(uAutenticacao.Token))
+                {
+                    string mensagem = $"Bem-vindo(a) {uAutenticacao.Username}.";
+
+                    Preferences.Set("UsuarioId", uAutenticacao.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticacao.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticacao.Perfil);
+                    Preferences.Set("UsuarioId", uAutenticacao.Token);
+
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", mensagem, "Ok");
+
+                    Application.Current.MainPage = new MainPage();
+                }
+                else
+                {
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", "Dados incorretos :(", "Ok");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
